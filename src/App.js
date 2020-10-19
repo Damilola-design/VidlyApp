82af7e122145ddcb7dab826baf0a9a8a8124042e
USERS <oquadri2014@gmail.com>
2020-10-19 07:15:42 +0100

    fifth commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import Movies from "./components/movies";
+import MovieForm from './components/MovieForm';
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from './components/navBar';
+import LoginForm from './components/loginForm';
+import './App.css';
+import RegisterForm from './components/registerForm';
+
+
+
+
+class App extends Component {
+  render() { 
+    return ( 
+      <React.Fragment>
+     <NavBar />
+    <main className="container">
+    <Switch>
+    <Route path="/register" component={RegisterForm} />
+    <Route path="/login" component={LoginForm} />
+    <Route path="/movies/:id" component={MovieForm} />
+    <Route path="/movies" component={Movies}></Route>
+    <Route path="/customers" component={Customers}></Route>
+    <Route path="/rentals" component={Rentals}></Route>
+    <Route path="/notFound" component={NotFound}></Route>
+    <Redirect from="/" exact to="/movies" />
+    <Redirect to="/notFound" />
+    </Switch>
+  </main> 
+  </React.Fragment>
+  );
+  }
+}
+ 
+export default App;
+
